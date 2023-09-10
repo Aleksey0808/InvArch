0cbbcaa43b620370e263b8fbedf926bc3de20ba7
Aleksey0808 <103374981+Aleksey0808@users.noreply.github.com>
2023-09-10 22:45:22 +0300

    fix menu-burger

--- src/js/burger-menu.js
+++ src/js/burger-menu.js
@@ -1,26 +1,30 @@
 (() => {
   const refs = {
+    openArrowUp: document.querySelector('[data-arrow-open]'),
+    openArrowClose: document.querySelector('[data-arrowClose-open]'),
     openMenuBtn: document.querySelector('[data-menu-open]'),
     closeMenuBtn: document.querySelector('[data-menu-close]'),
-    closMenuOpenModal: document.querySelector('[data-menu-close-open-modal]'),
 
-    closeMenuBtn1: document.querySelector('[data-menu-close-open-modal1]'),
-    closeMenuBtn2: document.querySelector('[data-menu-close-open-modal2]'),
-    closeMenuBtn3: document.querySelector('[data-menu-close-open-modal3]'),
-    closeMenuBtn4: document.querySelector('[data-menu-close-open-modal4]'),
     menu: document.querySelector('[data-menu]'),
+    arrow: document.querySelector('[data-arrow]'),
+    arrowClose: document.querySelector('[data-arrowClose]'),
   };
 
+  refs.openArrowUp.addEventListener('click', toggleArrow);
+  refs.openArrowUp.addEventListener('click', toggleArrowClose);
   refs.openMenuBtn.addEventListener('click', toggleModal);
   refs.closeMenuBtn.addEventListener('click', toggleModal);
   refs.closMenuOpenModal.addEventListener('click', toggleModal);
 
-  refs.closeMenuBtn1.addEventListener('click', toggleModal);
-  refs.closeMenuBtn2.addEventListener('click', toggleModal);
-  refs.closeMenuBtn3.addEventListener('click', toggleModal);
-  refs.closeMenuBtn4.addEventListener('click', toggleModal);
-
   function toggleModal() {
     refs.menu.classList.toggle('is-open');
+  }
+
+  function toggleArrow() {
+    refs.arrow.classList.toggle('is-hidden');
+  }
+
+  function toggleArrowClose() {
+    refs.arrowClose.classList.toggle('is-close');
   }
 })();
