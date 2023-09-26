cc1a3bd4f80cfe26efd43e0f2dc81439b2dfa149
Aleksey <103374981+Aleksey0808@users.noreply.github.com>
2023-09-26 15:46:13 +0300

    bth burger

--- src/js/burger-menu.js
+++ src/js/burger-menu.js
@@ -4,10 +4,12 @@
     openArrowClose: document.querySelector('[data-arrowClose-open]'),
     openMenuBtn: document.querySelector('[data-menu-open]'),
     closeMenuBtn: document.querySelector('[data-menu-close]'),
+    // openBurgerBtn: document.querySelector('[data-burger-open]'),
 
     menu: document.querySelector('[data-menu]'),
     arrow: document.querySelector('[data-arrow]'),
     arrowClose: document.querySelector('[data-arrowClose]'),
+    // burger: document.querySelector('[data-burger]'),
   };
 
   refs.openArrowUp.addEventListener('click', toggleArrow);
@@ -15,10 +17,16 @@
   refs.openMenuBtn.addEventListener('click', toggleModal);
   refs.closeMenuBtn.addEventListener('click', toggleModal);
   refs.closMenuOpenModal.addEventListener('click', toggleModal);
+  // refs.openBurgerBtn.addEventListener('click', toggleBurger);
 
   function toggleModal() {
-    refs.menu.classList.toggle('is-open');
+    refs.menu.classList.toggle('is-burger');
   }
+
+  // function toggleBurger() {
+  //   refs.burger.classList.toggle('is-open');
+  // }
+
 
   function toggleArrow() {
     refs.arrow.classList.toggle('is-hidden');
