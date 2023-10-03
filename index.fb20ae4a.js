5468f168c3eaf9561c61288129790f59326ed21c
Aleksey0808 <103374981+Aleksey0808@users.noreply.github.com>
2023-10-03 07:34:57 +0000

    Deploying to gh-pages from @ Aleksey0808/InvArch@7fe77d0d4ec84c05c58d740db174c059a702badd 🚀

---
+++ index.fb20ae4a.js
@@ -0,0 +1,2 @@
+!function(){var e={openBurger:document.querySelector("[data-burger-open]"),closeBurger:document.querySelector("[data-burger-close]"),burger:document.querySelector("[data-burger]")};function r(){e.burger.classList.toggle("is-burger")}e.openBurger.addEventListener("click",r),e.closeBurger.addEventListener("click",r)}();
+//# sourceMappingURL=index.fb20ae4a.js.map
