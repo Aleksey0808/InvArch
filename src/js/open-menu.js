8e55d0b75b9c54db968c910319658b3eda1ff3f6
Aleksey <103374981+Aleksey0808@users.noreply.github.com>
2023-10-10 15:46:11 +0300

    web

--- src/js/open-menu.js
+++ src/js/open-menu.js
@@ -1,26 +1,18 @@
 (() => {
   const refs = {
     openArrowUp: document.querySelector('[data-arrow-open]'),
-    openBurger: document.querySelector('[data-burger-open]'),
     openArrowClose: document.querySelector('[data-arrowClose-open]'),
     openMenuBtn: document.querySelector('[data-menu-open]'),
-    closeBurger: document.querySelector('[data-burger-close]'),
 
     menu: document.querySelector('[data-menu]'),
     arrow: document.querySelector('[data-arrow]'),
     arrowClose: document.querySelector('[data-arrowClose]'),
-    // burger: document.querySelector('[data-burger]'),
   };
 
   refs.openArrowUp.addEventListener('click', toggleArrow);
-  // refs.openBurger.addEventListener('click', toggleBurger);
   refs.openArrowUp.addEventListener('click', toggleArrowClose);
   refs.openMenuBtn.addEventListener('click', toggleModal);
   refs.closeBurger.addEventListener('click', toggleBurger);
-
-  // function toggleBurger() {
-  //   refs.burger.classList.toggle('is-burger');
-  // }
 
   function toggleModal() {
     refs.menu.classList.toggle('is-open');
